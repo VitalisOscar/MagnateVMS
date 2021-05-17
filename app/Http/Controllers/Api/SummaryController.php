20a90ac834a74d671b949cce52dff07af67b14d2
VitalisOscar <vitalisoscar25@gmail.com>
2021-05-17 09:19:42 +0300

    Visitors

--- app/Http/Controllers/Api/SummaryController.php
+++ app/Http/Controllers/Api/SummaryController.php
@@ -23,18 +23,30 @@ class SummaryController extends Controller
         $q->stillIn()->atSite()->today();
     })->count();
 
-        return $this->json->data(['visitors' => [
-	    ['name'=>'John Doe', 'time'=>'08:44', 'about'=>'Visitor', 'activity'=>'check_in', 'type'=>'visitor', 'data' => ''],
-		['name'=>'Calvin Harris', 'time'=>'08:50', 'about'=>'Visitor', 'activity'=>'check_in', 'type'=>'visitor', 'data' => ''],
-		['name'=>'Bob Stuart', 'time'=>'08:53', 'about'=>'Visitor', 'activity'=>'check_out', 'type'=>'visitor', 'data' => ''],
-		['name'=>'Jane Atieno', 'time'=>'09:00', 'about'=>'Visitor', 'activity'=>'check_out', 'type'=>'visitor', 'data' => ''],
-		['name'=>'Allan Munyika', 'time'=>'09:05', 'about'=>'Visitor', 'activity'=>'check_in', 'type'=>'visitor', 'data' => ''],
-	], 'summary' => [
+	$visitor_list = Visitor::whereHas('visit', function($q){
+	    $q->today()->latest('time_in');
+	});
+
+	$visitor_list = Visit::latest('time_in')->limit(5)->with('visitor')->each(function($v){
+	    $v->activity = ($v->time_out == null) ? 'check_in':'check_out';
+	    $v->name = $v->visitor->name;
+	    $v->data = $visitor;
+	});
+
+        return $this->json->data(['visitors' => $visitors, 'summary' => [
 		'checked_in' => $visitors_in,
 		'checked_out' => $visitors_out,
 		'staff' => $staff
 	    ]
     	]);
+
+	/*[
+	    ['name'=>'John Doe', 'time'=>'08:44', 'about'=>'Visitor', 'activity'=>'check_in', 'type'=>'visitor', 'data' => ''],
+		['name'=>'Calvin Harris', 'time'=>'08:50', 'about'=>'Visitor', 'activity'=>'check_in', 'type'=>'visitor', 'data' => ''],
+		['name'=>'Bob Stuart', 'time'=>'08:53', 'about'=>'Visitor', 'activity'=>'check_out', 'type'=>'visitor', 'data' => ''],
+		['name'=>'Jane Atieno', 'time'=>'09:00', 'about'=>'Visitor', 'activity'=>'check_out', 'type'=>'visitor', 'data' => ''],
+		['name'=>'Allan Munyika', 'time'=>'09:05', 'about'=>'Visitor', 'activity'=>'check_in', 'type'=>'visitor', 'data' => ''],
+	],*/
 
     }
 
