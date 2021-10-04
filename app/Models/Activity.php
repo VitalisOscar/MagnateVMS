508a7a420c084cbb96c4b8a6001c6bdc5bc1b1fa
VitalisOscar <vitalisoscar25@gmail.com>
2021-10-04 20:27:35 +0300

    fix error

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -27,7 +27,7 @@ class Activity extends Model
         'time' => 'datetime'
     ];
 
-    protected $appends = ['fmt_time', 'fmt_date']
+    protected $appends = ['fmt_time', 'fmt_date'];
 
     // Relations
     function user(){ return $this->belongsTo(User::class); }
